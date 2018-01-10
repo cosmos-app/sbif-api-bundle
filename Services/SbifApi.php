c1eb6d3dd3b23444d84a4719b0f9d628054e1e56
Héctor D Rojas S <hector.d.rojas.s@gmail.com>
2018-01-10 00:35:53 -0300

    Se añade estructura

---
+++ Services/SbifApi.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the SbifApiBundle package.
+ *
+ * (c) Cosmos App <https://github.com/cosmos-app>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CosmosApp\SbifApiBundle\Services;
+
+use GuzzleHttp\Client;
+
+/**
+ * @author Héctor Rojas <hector.d.rojas.s@gmail.com>
+ */
+class SbifApi
+{
+}
